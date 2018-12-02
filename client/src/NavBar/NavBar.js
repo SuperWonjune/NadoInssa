ffc2a7a6a71c2aa6b9fb410f563badedb5b7f462
Superwonjune <avgnrolfe@naver.com>
2018-12-03 01:19:22 +0900

    NavBar CSS Added

--- client/src/NavBar/NavBar.js
+++ client/src/NavBar/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import NameSearch from './NameSearch';
+import './NavBar.css';
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-dark bg-primary fixed-top">
-      <Link className="navbar-brand mb-0 h1" to="/">
+    <nav className="navbar navbar-inverse bg-primary fixed-top">
+      <Link className="navbar-brand" to="/">
         인싸 위키
       </Link>
       <NameSearch/> 
